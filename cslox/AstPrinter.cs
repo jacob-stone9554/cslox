c5f84bb15531283bc906c462912638d8cf4212bb
Jacob Stone <jumboshrimp9554@gmail.com>
2026-06-11 21:01:11 -0400

    Created a simple pretty printer to evaulate expression heirarchies

---
+++ cslox/AstPrinter.cs
@@ -0,0 +1,49 @@
+﻿using System.Text;
+using Microsoft.VisualBasic.CompilerServices;
+
+namespace cslox;
+
+public class AstPrinter : Expr.IVisitor<string>
+{
+
+    public string Print(Expr expr)
+    {
+        return expr.Accept(this);
+    }
+    public string VisitBinaryExpr(Binary expr)
+    {
+        return Parenthesize(expr.op.lexeme, expr.left, expr.right);
+    }
+
+    public string VisitGroupingExpr(Grouping expr)
+    {
+        return Parenthesize("group", expr.expression);
+    }
+
+    public string VisitLiteralExpr(Literal expr)
+    {
+        if (expr.value == null) return "nil";
+        return expr.value.ToString();
+    }
+
+    public string VisitUnaryExpr(Unary expr)
+    {
+        return Parenthesize(expr.op.lexeme, expr.right);
+    }
+
+    private string Parenthesize(string name, params Expr[] exprs)
+    {
+        StringBuilder builder = new StringBuilder();
+
+        builder.Append("(").Append(name);
+
+        foreach (var expr in exprs)
+        {
+            builder.Append(" ");
+            builder.Append(expr.Accept(this));
+        }
+        
+        builder.Append(")");
+        return builder.ToString();
+    }
+}
